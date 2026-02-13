9af6a13bb454fc15e98aad181dd6dae033627cdb
Shreyash_Raut <rshreyash784@gmail.com>
2026-02-13 23:10:20 +0530

    Updated the lib.rs, take.rs and refund.rs

--- programs/anchor-escrow-program/src/instructions/refund.rs
+++ programs/anchor-escrow-program/src/instructions/refund.rs
@@ -42,7 +42,7 @@ pub struct Refund<'info> {
 }
 
 impl<'info> Refund<'info> {
-    pub fn refund_and_clode_vault(&mut self) -> Result<()> {
+    pub fn refund_and_close_vault(&mut self) -> Result<()> {
         let signer_seeds:&[&[&[u8]]] = &[&[b"escrow", self.maker.to_account_info().key.as_ref(), &self.escrow.seed.to_le_bytes(), &[self.escrow.bump],]]; 
         
         let transfer_accounts = TransferChecked{
@@ -56,10 +56,9 @@ impl<'info> Refund<'info> {
 
         transfer_checked(transfer_cpi_ctx, self.vault.amount, self.mint_a.decimals)?;
 
-        let close_accounts = TransferChecked {
-            from : self.vault.to_account_info(),
-            mint : self.mint_a.to_account_info(),
-            to : self.maker_ata_a.to_account_info(),
+        let close_accounts = CloseAccount {
+            account : self.vault.to_account_info(),
+            destination : self.maker.to_account_info(), 
             authority : self.escrow.to_account_info()
         };
 
@@ -67,5 +66,4 @@ impl<'info> Refund<'info> {
 
         close_account(close_cpi_ctx)
     }
-
 } 
